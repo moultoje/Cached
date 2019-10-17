9dc9a9b12fc45ca1ed9712e06254f252af552688
jeff <jeff@jeffs-MacBook-Pro.local>
2019-10-16 21:49:37 -0600

    Cached Xcode project creation and initial view layout

---
+++ Cached/Cached/HuntsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Cached
+//
+//  Created by jeff on 10/16/19.
+//  Copyright © 2019 Mobile Treasure Hunt. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Cached/Cached/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Cached
+//
+//  Created by jeff on 10/16/19.
+//  Copyright © 2019 Mobile Treasure Hunt. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
