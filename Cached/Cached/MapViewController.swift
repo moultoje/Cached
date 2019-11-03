069071f4aeb007773b15e8acc8ab6a9e8dc72907
Drew Hawthorne <hawthord@oregonstate.edu>
2019-11-03 07:57:20 -0800

    Detects location services, prompts user if not enabled.

--- Cached/Cached/MapViewController.swift
+++ Cached/Cached/MapViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        checkLocationServices()
+        //mapView.delegate = self
     }
-
-
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            checkLocationAuthorization()
+        } else {
+            print("Please enable location services in iPhone Settings.")
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse: break //mapView.showsUserLocation = true
+            case .denied: print("Please enable location services in iPhone Settings.")
+            case .notDetermined: break
+            case .restricted: break
+            case .authorizedAlways: break
+        }
+    }
 }
 
