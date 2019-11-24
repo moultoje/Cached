9951e3cb4889d1b4bc26cc1c0da322b65f760b73
Katie Rievley <katierievley@katies-macbook-pro.local>
2019-11-24 11:26:18 -0800

    added selection of public hunts, unwinds to map view and pass selected hunt data

--- Cached/Cached/Waypoint.swift
+++ Cached/Cached/Waypoint.swift
@@ -10,25 +10,31 @@ import Foundation
 import UIKit
 
 struct Waypoint {
+    var name: String
     var clue: String
     var latitude: Double
     var longitude: Double
+    var radius: Int
     var id: String?
     
     var dictionary:[String:Any]{
         return [
+            "name": name,
             "clue": clue,
             "latitude": latitude,
-            "longitude": longitude
+            "longitude": longitude,
+            "radius": radius
         ]
     }
 }
 
 extension Waypoint {
     init(dictionary: [String:Any], id: String) {
+        name = dictionary["name"] as? String ?? ""
         clue = dictionary["clue"] as? String ?? ""
         latitude = dictionary["latitude"] as? Double ?? 0
         longitude = dictionary["longitude"] as? Double ?? 0
+        radius = dictionary["radius"] as? Int ?? 0
         
         /*
          self.init(name: name, isPrivate: isPrivate, creator: creator, description: description, generalLocation: generalLocation, numberWaypoints: numberWaypoints, listWaypoints: listWaypoints, id: id)
