fb7e8290af956d865a8ac14a8beb5ef185b29e36
Jeffrey Moulton <jeff@Jeffreys-MacBook-Air.local>
2019-11-30 16:23:39 -0700

    Updated found button logic and button for switching between standard and sattelite views

--- Cached/Cached/View Controllers/MapViewController.swift
+++ Cached/Cached/View Controllers/MapViewController.swift
@@ -21,7 +21,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     
     var currentHunt: Hunt = Hunt(dictionary:["String":""],id:"")
     public var waypoints: [Waypoint] = []
-    var waypointIndex = 1;
+    var waypointIndex = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +36,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         super.viewWillAppear(true)
         
         if(!waypoints.isEmpty){
-            clueText.text = waypoints[waypointIndex-1].clue
+            clueText.text = waypoints[waypointIndex].clue
         }
         
         print(currentHunt)
@@ -67,6 +67,9 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         }
     }
     
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+    }
+    
     @IBAction func center(_ sender: Any) {
         if let userLocation = mapView.userLocation.location?.coordinate {
             
@@ -77,11 +80,14 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         }
     }
     
+    @IBOutlet weak var changeMapTypeText: UIButton!
     @IBAction func changeMapType(_ sender: Any) {
         if mapView.mapType == MKMapType.standard {
             mapView.mapType = MKMapType.satellite
+            changeMapTypeText.setTitle("Standard", for: UIControl.State.normal)
         } else {
             mapView.mapType = MKMapType.standard
+            changeMapTypeText.setTitle("Terrain", for: UIControl.State.normal)
         }
     }
     
@@ -89,16 +95,55 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     
     //MARK: Found Button
     @IBAction func foundButton(_ sender: Any) {
-        //Waypoint found, increment waypointIndex and update clue text.
-        waypointIndex += 1
-        clueText.text = waypoints[waypointIndex-1].clue
-        
-        //Waypoint not found
-        let alert = UIAlertController(title: "Outside of Waypoint Radius", message: "You have not found the waypoint yet.", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
-        NSLog("The \"Waypoint Not Found\" alert occured.")
-        }))
-        self.present(alert, animated: true, completion: nil)
+        //Get the user's location
+        if waypoints.isEmpty{
+            let alert = UIAlertController(title: "No Hunt Started", message: "No treasure hunt has been started yet.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"Hunt Not Started\" alert occured.")
+            }))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            let userLoc = locationManager.location
+            let waypointLoc = CLLocation(latitude: waypoints[waypointIndex].latitude, longitude: waypoints[waypointIndex].longitude)
+            var userDist:CLLocationDistance
+            
+            if userLoc == nil{
+                let alert = UIAlertController(title: "User Location Not Determined", message: "Either location services are turned off or the app failed to update your location. If location services are on, try again.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                NSLog("The \"Location Undetermined\" alert occured.")
+                }))
+                self.present(alert, animated: true, completion: nil)
+                userDist = Double.infinity
+            } else {
+                userDist = waypointLoc.distance(from: userLoc!)
+            }
+            
+            if userDist < Double(waypoints[waypointIndex].radius) {
+                //Waypoint found, increment waypointIndex and update clue text.
+                waypointIndex += 1
+                if waypointIndex == waypoints.count {
+                    // Reset waypointIndex and waypoint array
+                    waypointIndex = 0
+                    waypoints = []
+                    clueText.text = ""
+                    // Congratulate user on winning
+                    let alert = UIAlertController(title: "Treasure Hunt Complete", message: "You have found all of the waypoints. Congratulations!", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                    NSLog("The \"Hunt Complete\" alert occured.")
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    clueText.text = waypoints[waypointIndex].clue
+                }
+            } else if userDist != Double.infinity {
+                //Waypoint not found
+                let alert = UIAlertController(title: "Outside of Waypoint Radius", message: "You have not found the waypoint yet.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                NSLog("The \"Waypoint Not Found\" alert occured.")
+                }))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
     }
     
 }
